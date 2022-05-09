d4eeef9dcdc70ec2ca12e4ce7152e73c04a9a90d
Caleb Gore <cgore2001@gmail.com>
2022-05-09 14:05:04 -0500

    added comments

--- scripts/Minerals.js
+++ scripts/Minerals.js
@@ -1,12 +1,16 @@
 import { getColonyMineralJoins, getMinerals } from "./database.js";
 
-
-export const CurrentColonyMinerals = (colony) => {
+// export function to create current minerals html
+export const CurrentColonyMinerals = (colony) => { // takes selectedColony from Governors.js eventListener as parameter
+    // assign imported data to variables
     const joins = getColonyMineralJoins()
     const minerals = getMinerals()
+    // declare empty string
     let html = ""
+    // iterate all colony-mineral joins
     joins.forEach((join) => {
         if (join.colonyId === colony.id) {
+            // find mineral that matches join
             const mineral = minerals.find(m => m.id === join.mineralId)
             html += `<p>${join.tons} tons of ${mineral.name}</p>`
         }
